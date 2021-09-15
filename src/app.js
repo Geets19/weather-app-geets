5fb3a0be337e72b66e09278045c58b2d13572745
Geets19 <geets19@hotmail.com>
2021-09-15 13:10:49 +0100

    Added search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "dc0bef5554f93515a80bed3b8c51878a";
-let city = "Kuala Lumpur";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "dc0bef5554f93515a80bed3b8c51878a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Kuala Lumpur");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
